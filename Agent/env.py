587094c31b955bbcddfd979a3438fc651e70a07a
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2024-04-03 14:46:01 +0700

    create env.py
    + create environment for Deep Q learning
    + update requirements.txt

---
+++ Agent/env.py
@@ -0,0 +1,67 @@
+import gym
+from gym import spaces
+import numpy as np
+from Utils.get_data import get_data
+class CustomEnv(gym.Env):
+    def __init__(self):
+        super(CustomEnv, self).__init__()
+        self.observation_space = spaces.Discrete(5)  # Example discrete state space
+        self.action_space = spaces.Discrete(3)  # Three discrete actions: 0, 1, 2
+        self.timestep=200
+        self.state = get_data.iloc[self.timestep:self.timestep+42]
+        self.von = 1000
+        self.portfolio = 1000
+    def reset(self):
+        self.timestep = 200
+        self.state = get_data.iloc[self.timestep:self.timestep+42]  # Reset the state
+        self.von = 1000
+        self.portfolio = 1000
+        self.inventory = []
+        return self.state
+
+    def step(self, action):
+        assert self.action_space.contains(action), "Invalid action"
+
+        # Perform action and update state
+        if action == 0:
+            self.timestep += 1
+            self.state = get_data.iloc[self.timestep:self.timestep+42]
+        elif action == 1:
+            self.timestep += 1
+            self.state = get_data.iloc[self.timestep:self.timestep+42]
+            self.inventory.append((self.von/10)/self.state.iloc[:-1]['cl'])
+            self.von -= self.von/10
+            
+        else:
+            self.timestep += 1
+            self.state = get_data.iloc[self.timestep:self.timestep+42]
+            self.von += self.inventory[-1] * self.state.iloc[:-1]['cl']
+            self.inventory = self.inventory[:-1]
+
+        # Define reward based on the new state
+        reward = self._calculate_reward()
+
+        # Check if episode is done
+        done = self._is_done()
+
+        # Define observation (state) for the next step
+        next_observation = self.state
+
+        return next_observation, reward, done
+
+    def _calculate_reward(self):
+        portfo = 0
+        for each in self.inventory:
+            each *= self.state.iloc[:-1]['cl']
+            portfo += each
+        portfo += self.von
+        reward = portfo - self.portfolio
+        self.portfolio = portfo
+        
+        print('reward:',reward)
+        return reward  # No reward otherwise
+
+    def _is_done(self):
+        # Define termination condition
+        return self.timestep >= 500*29 + 10
+
