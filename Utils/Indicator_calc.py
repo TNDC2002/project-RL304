3c7568ba65224036b1881f7ab04e7f656553152a
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2024-04-03 12:35:17 +0700

    Update Indicator_calc.py
    fix indicator discountinue issue

--- Utils/Indicator_calc.py
+++ Utils/Indicator_calc.py
@@ -4,10 +4,8 @@ from get_data import get_data
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 import os
-def calced(file_path,filename):
+def calced(file_path):
     data = pd.read_csv(file_path)
-    if not filename.endswith('.csv'):
-        return False
     if 'Doji' in data.columns:
         # có doji => ko cho chạy
         print("=========shouldn't run==========")
@@ -21,16 +19,46 @@ def calc(path = '../Data'):
     # Iterate over files in the folder
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
-        if calced(file_path=file_path,filename=filename):
-        
+        if filename.endswith('.csv'):
             # Read CSV file into DataFrame and append to the list
             data = pd.read_csv(file_path)
             data.dropna(axis=0, how='all', inplace=True)
-    
+            
     
             # Sort data by date if it's not already sorted
             data = data.sort_values(by='timestamp_o')
+            data_len = len(data)
             
+            # # Pattern Recognition
+            if calced(file_path=file_path):
+                    # Doji
+                data['Doji'] = talib.CDLDOJI(data['o'], data['h'], data['l'], data['cl'])
+                    # Dragonfly Doji
+                data['Dragonfly_Doji'] = talib.CDLDRAGONFLYDOJI(data['o'], data['h'], data['l'], data['cl'])
+                    # Gravestone Doji
+                data['Gravestone_Doji'] = talib.CDLGRAVESTONEDOJI(data['o'], data['h'], data['l'], data['cl'])
+                    # Marubozu
+                data['Marubozu'] = talib.CDLMARUBOZU(data['o'], data['h'], data['l'], data['cl'])
+                    # Harami Pattern
+                data['Harami'] = talib.CDLHARAMI(data['o'], data['h'], data['l'], data['cl'])
+                    # Hammer
+                data['Hammer'] = talib.CDLHAMMER(data['o'], data['h'], data['l'], data['cl'])
+                    # Inverted Hammer
+                data['Inverted_Hammer'] = talib.CDLINVERTEDHAMMER(data['o'], data['h'], data['l'], data['cl'])
+                    # Engulfing
+                data['Engulfing'] = talib.CDLENGULFING(data['o'], data['h'], data['l'], data['cl'])
+                    # Morning Star
+                data['Morning_Star'] = talib.CDLMORNINGSTAR(data['o'], data['h'], data['l'], data['cl'])
+                    # Evening Star
+                data['Evening_Star'] = talib.CDLEVENINGSTAR(data['o'], data['h'], data['l'], data['cl'])
+                    # Hanging Man
+                data['Hanging_Man'] = talib.CDLHANGINGMAN(data['o'], data['h'], data['l'], data['cl'])
+                    # Dark Cloud Cover
+                data['Dark_Cloud_Cover'] = talib.CDLDARKCLOUDCOVER(data['o'], data['h'], data['l'], data['cl'])
+
+
+            dfs.append(data)
+            data = pd.concat(dfs, ignore_index=False)
 
             # # Trend indicator (OVERLAP STUDIES)
                 # Calculate Moving Average (MA)
@@ -82,39 +110,8 @@ def calc(path = '../Data'):
             #     ATR                  
             data['ATR'] = talib.ATR(data['h'], data['l'], data['cl'], timeperiod=14)
 
-            # # Pattern Recognition
-                # Doji
-            data['Doji'] = talib.CDLDOJI(data['o'], data['h'], data['l'], data['cl'])
-                # Dragonfly Doji
-            data['Dragonfly_Doji'] = talib.CDLDRAGONFLYDOJI(data['o'], data['h'], data['l'], data['cl'])
-                # Gravestone Doji
-            data['Gravestone_Doji'] = talib.CDLGRAVESTONEDOJI(data['o'], data['h'], data['l'], data['cl'])
-                # Marubozu
-            data['Marubozu'] = talib.CDLMARUBOZU(data['o'], data['h'], data['l'], data['cl'])
-                # Harami Pattern
-            data['Harami'] = talib.CDLHARAMI(data['o'], data['h'], data['l'], data['cl'])
-                # Hammer
-            data['Hammer'] = talib.CDLHAMMER(data['o'], data['h'], data['l'], data['cl'])
-                # Inverted Hammer
-            data['Inverted_Hammer'] = talib.CDLINVERTEDHAMMER(data['o'], data['h'], data['l'], data['cl'])
-                # Engulfing
-            data['Engulfing'] = talib.CDLENGULFING(data['o'], data['h'], data['l'], data['cl'])
-                # Morning Star
-            data['Morning_Star'] = talib.CDLMORNINGSTAR(data['o'], data['h'], data['l'], data['cl'])
-                # Evening Star
-            data['Evening_Star'] = talib.CDLEVENINGSTAR(data['o'], data['h'], data['l'], data['cl'])
-                # Hanging Man
-            data['Hanging_Man'] = talib.CDLHANGINGMAN(data['o'], data['h'], data['l'], data['cl'])
-                # Dark Cloud Cover
-            data['Dark_Cloud_Cover'] = talib.CDLDARKCLOUDCOVER(data['o'], data['h'], data['l'], data['cl'])
 
-
-
-            patterns = ['Doji', 'Dragonfly_Doji', 'Gravestone_Doji', 'Marubozu', 'Harami', 'Hammer', 
-                        'Inverted_Hammer', 'Engulfing', 'Morning_Star', 'Evening_Star', 'Hanging_Man', 'Dark_Cloud_Cover']
-
-            dfs.append(data)
-            data.to_csv(file_path, index=True)
+            data.tail(data_len).to_csv(file_path, index=True)
 
     
     if len(dfs) != 0:    
---
+++ Utils/data_processing.py
@@ -0,0 +1,8 @@
+from get_data import RequestAndSavedata
+from Indicator_calc import calc
+
+# Step 1
+# RequestAndSavedata()
+
+# Step 2
+calc()
